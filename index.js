1ee484e25402dde796807e62533e43aa23699090
Kenneth Candelaria <ddevkenmc@gmail.com>
2021-09-03 15:02:17 +0800

    added eslint

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 
+const {ESLint} = require('eslint');
+
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
-
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
+});
+
+
+(async function main () {
+    const eslint = new ESLint();
+
+    const results = await eslint.lintFiles(['/index.js']);
+
+    const formatter = await eslint.loadFormatter('stylish');
+    const resultText = formatter.format(results);
+
+    console.log(resultText);
+}()).catch((error) => {
+    process.exitCode = 1;
+    console.error(error);
 });
